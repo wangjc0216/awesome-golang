5619192cd745fcfb76141b2d24627a362ca70065
wangjc <wangjc0216@outlook.com>
2021-11-01 10:54:29 +0800

    add context

---
+++ example/context/example.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	exampleWithContext()
+}
+
+func exampleWithContext() {
+
+	const shortDuration = 1 * time.Millisecond
+
+	// Pass a context with a timeout to tell a blocking function that it
+	// should abandon its work after the timeout elapses.
+	ctx, cancel := context.WithTimeout(context.Background(), shortDuration)
+	defer cancel()
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("overslept")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err()) // prints "context deadline exceeded"
+	}
+
+}
